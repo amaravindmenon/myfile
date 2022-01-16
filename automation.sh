f0938970b118d961d787c4eb32a887733e4775f1
Aravind Menon <53946198+amaravindmenon@users.noreply.github.com>
2022-01-16 09:11:02 +0530

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -15,28 +15,6 @@ scan started for $i
 "
 
 mkdir bugs/$i
-subfinder -d $i | httpx >> bugs/$i/subdomains.txt
-
-echo "Subdomains Saved"
-echo "scan for cves started"
-nuclei -l bugs/$i/subdomains.txt -t nuclei-templates/cves -o bugs/$i/cves.txt
-echo "scan for vulnerabilities started"
-nuclei -l bugs/$i/subdomains.txt -t nuclei-templates/vulnerabilities -o bugs/$i/vuln.txt
-echo "scan for takeover started"
-nuclei -l bugs/$i/subdomains.txt -t nuclei-templates/takeovers -o bugs/$i/takeover.txt
-echo "scan for exposures started"
-nuclei -l bugs/$i/subdomains.txt -t nuclei-templates/exposures -o bugs/$i/exposures.txt
-echo "scan for fuzz started"
-nuclei -l bugs/$i/subdomains.txt -t nuclei-templates/fuzzing -o bugs/$i/fuzz.txt
-echo "scan for panel started"
-nuclei -l bugs/$i/subdomains.txt -t nuclei-templates/exposed-panels -o bugs/$i/panel.txt
-echo "scan for default-login started"
-nuclei -l bugs/$i/subdomains.txt -t nuclei-templates/default-logins -o bugs/$i/login.txt
-echo "scan for misconf started"
-nuclei -l bugs/$i/subdomains.txt -t nuclei-templates/misconfiguration -o bugs/$i/misconf.txt
-echo "scan for misconf started"
-nuclei -l bugs/$i/subdomains.txt -t nuclei-templates/file -o bugs/$i/files.txt
-echo "Nuclei scan completed"
 
 echo "XSS Hunting using gf and dalfox and xsstrike"
 waybackurls $i > bugs/$i/temp_waybackxss.txt
