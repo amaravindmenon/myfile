c996c5b63e044c2a3716da1684c73b5ec4e70a31
Aravind Menon <53946198+amaravindmenon@users.noreply.github.com>
2022-01-19 15:34:07 +0530

    Create dalfox.py

---
+++ dalfox.py
@@ -0,0 +1,25 @@
+#!usr/bin/bash
+
+echo " My script ";
+
+read -p "Enter domain name => " input
+for i in ${input[@]}
+do
+echo "
+.
+.
+.
+scan started for $i
+"
+
+mkdir bugs/$i
+subfinder -d $i | httpx >> bugs/$i/subdomains.txt
+
+echo "XSS Hunting using gf and dalfox and xsstrike"
+waybackurls $i > bugs/$i/temp_waybackxss.txt
+cat bugs/$i/temp_waybackxss.txt | gf xss | sed 's/=.*/=/' | egrep -iv ".(jpg|jpeg|gif|tif|tiff|ico|pdf|svg|png|css|woff|woff2)" > bugs/$i/temp2_waybackxss.txt
+sort bugs/$i/temp2_waybackxss.txt | uniq -i > bugs/$i/waybackxss.txt
+cat bugs/$i/waybackxss.txt | dalfox pipe -o bugs/$i/dalfoxoutput.txt
+echo "XSSATACK FINISH"
+
+done
